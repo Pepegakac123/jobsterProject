28a7d2ae7a4d7038cbe8933b97e7551d6d12b466
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-01-21 19:33:29 +0100

    Added Toaster from shadCn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import Landing from "./pages/Landing";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { Toaster } from "@/components/ui/toaster";
 const App = () => {
-	return <RouterProvider router={router} />;
+	return (
+		<>
+			<RouterProvider router={router} />
+			<Toaster />
+		</>
+	);
 };
 export default App;
